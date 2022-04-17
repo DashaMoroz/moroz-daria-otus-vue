02b8710a0c9ecb4a0d5ddaa585172d79970e2240
darya moroz <darya.moroz@etalongroup.com>
2022-04-17 20:49:33 +0300

    vue-1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // базовый путь
+  publicPath: './',
+  // каталог вывода файлов
+  outputDir: 'dist',
+  // каталог для хранения сгенерированных статических ресурсов
+  assetsDir: '',
+  // путь к сгенерированному index.html
+  indexPath: 'index.html',
+  css: {
+    // использование source maps для CSS
+    sourceMap: false,
+  },
 })
